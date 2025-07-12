a32b6f463542c7454fb3ddf131b0e30cbbd3c9b3
Teo Fernández <81999491+TeoFernandez@users.noreply.github.com>
2025-07-12 18:19:08 -0300

    Agregar archivos de inicio de sesión: HTML y CSS para la interfaz de usuario

---
+++ login/login.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="../css/login.css">
+    <title>Login - Sistema de Stock</title>
+</head>
+<body>
+    <div class="login-container">
+        <h2>Iniciar sesión</h2>
+        
+        <form action="../Back-end/validar_login.php" method="POST">
+            <label>Usuario:</label><br>
+            <input type="text" name="usuario" required><br><br>
+
+            <label>Contraseña:</label><br>
+            <input type="password" name="clave" required><br><br>
+
+            <input type="submit" value="Ingresar">
+        </form>
+    </div>
+</body>
+</html>
